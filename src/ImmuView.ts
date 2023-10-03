f0e73aba930fea6d122f362a87e63e94ae5a1a92
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2023-10-04 00:37:38 +0200

    finally working version with complete tests

--- src/ImmuView.ts
+++ src/ImmuView.ts
@@ -4,7 +4,7 @@ class ValidationError extends Error {}
 type ErrorHandler = (error: Error) => void;
 
 type ReadonlyState<T> = {
-    value: Readonly<T>;
+    value: T;
     internalSet: (newValue: T) => void;
 };
 
@@ -16,94 +16,204 @@ interface ReadonlyStateOptions<T> {
     validationErrorMessage?: string;
 }
 
-const defaultErrorHandler = (error: Error) => {
+export const defaultErrorHandler = (error: Error) => {
     console.error(error.message);
 };
 
 const isObject = (value: any): value is object =>
     typeof value === 'object' && value !== null;
 
-const createInternalSet = <T>(
-    valueRef: { current: T },
-    validator?: Validator<T>,
-    errorHandler: ErrorHandler = defaultErrorHandler,
-    validationErrorMessage?: string
-) => {
-    return (newValue: T) => {
-        if (validator && !validator(newValue)) {
-            errorHandler(
-                new ValidationError(
-                    validationErrorMessage || 'Validation failed.'
-                )
+const deepMerge = (target: any, source: any) => {
+    for (const key in source) {
+        if (isObject(source[key])) {
+            if (!target[key]) target[key] = {};
+            deepMerge(target[key], source[key]);
+        } else {
+            target[key] = source[key];
+        }
+    }
+};
+
+const createDeepProxy = <T>(
+    target: T,
+    options?: ReadonlyStateOptions<T>,
+    proxies: WeakMap<object, any> = new WeakMap()
+): T => {
+    let proxyKey = {};
+    if (!isObject(target)) {
+        proxyKey = { value: target };
+    } else {
+        proxyKey = target;
+    }
+
+    if (proxies.has(proxyKey)) {
+        return proxies.get(proxyKey);
+    }
+
+    const handler: ProxyHandler<T & { [key: string]: any }> = {
+        get: (obj, prop: string | symbol) => {
+            console.log(
+                'GET TRAP',
+                'Accessing property',
+                prop,
+                'of object',
+                obj
             );
-            return;
-        }
-        valueRef.current = newValue;
+            const value = obj[prop as keyof T];
+            if ((value as any) instanceof Date) {
+                console.log('Date found');
+                const dateProxyHandler: ProxyHandler<Date> = {
+                    get: (dateObj, dateProp) => {
+                        if (
+                            [
+                                'setDate',
+                                'setFullYear',
+                                'setHours',
+                                'setMilliseconds',
+                                'setMinutes',
+                                'setMonth',
+                                'setSeconds',
+                                'setTime',
+                                'setUTCDate',
+                                'setUTCDay',
+                                'setUTCFullYear',
+                                'setUTCHours',
+                                'setUTCMilliseconds',
+                                'setUTCMinutes',
+                                'setUTCMonth',
+                                'setUTCSeconds',
+                            ].includes(String(dateProp))
+                        ) {
+                            return () => {
+                                throw new DirectMutationError(
+                                    'Cannot modify readonly state.'
+                                );
+                            };
+                        }
+                        return dateObj[dateProp as keyof Date];
+                    },
+                };
+                return new Proxy(value, dateProxyHandler);
+            } else if (
+                Array.isArray(value) ||
+                (Array.isArray(obj) && typeof value === 'function')
+            ) {
+                console.log('Array or array method found');
+                const arrayProxyHandler: ProxyHandler<any[]> = {
+                    get: (arr, arrProp) => {
+                        console.log(
+                            `Accessing property ${String(arrProp)} of array`,
+                            arr
+                        );
+
+                        if (
+                            [
+                                'push',
+                                'pop',
+                                'shift',
+                                'unshift',
+                                'splice',
+                                'reverse',
+                                'sort',
+                            ].includes(String(arrProp))
+                        ) {
+                            return () => {
+                                throw new DirectMutationError(
+                                    'Cannot modify readonly state.'
+                                );
+                            };
+                        }
+
+                        const item = arr[arrProp as any];
+                        console.log(`Item at index ${String(arrProp)}:`, item);
+
+                        if (isObject(item)) {
+                            return createDeepProxy(item as T, options, proxies);
+                        }
+                        return item;
+                    },
+                };
+                return new Proxy(value, arrayProxyHandler);
+            } else if (isObject(value)) {
+                console.log('Object found');
+                return createDeepProxy(value as T, options, proxies);
+            } else if (typeof value === 'function') {
+                console.log('Function found');
+                return value.bind(obj); // Bind the function to the original object
+            }
+            return value;
+        },
+        set: () => {
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
+        ownKeys: (obj) => {
+            return Reflect.ownKeys(obj);
+        },
+        has: (target, key) => {
+            return key in target;
+        },
+        getOwnPropertyDescriptor: (target, key) => {
+            return Reflect.getOwnPropertyDescriptor(target, key);
+        },
+        deleteProperty: () => {
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
+        defineProperty: () => {
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
+        setPrototypeOf: () => {
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
+        preventExtensions: () => {
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
+        isExtensible: () => {
+            return false;
+        },
+        getPrototypeOf: () => {
+            return isObject(target) ? Reflect.getPrototypeOf(target) : null;
+        },
+        apply: (target, _thisArg, _argumentsList) => {
+            if (typeof target !== 'function') {
+                throw new DirectMutationError('Target is not callable.');
+            }
+            throw new DirectMutationError('Cannot modify readonly state.');
+        },
     };
-};
 
-const objectHandler = <T extends object>(valueRef: {
-    current: T;
-}): ProxyHandler<T> => ({
-    get: (_target, property) => {
-        const propValue = valueRef.current[property as keyof T];
-        if (isObject(propValue)) {
-            return new Proxy(propValue, objectHandler({ current: propValue }));
-        }
-        return propValue;
-    },
-    set: (_target, property, _newValue) => {
-        throw new DirectMutationError(
-            `Cannot modify readonly state property '${String(property)}'.`
-        );
-    },
-});
-
-const createProxy = <T extends object>(
-    initialValue: T,
-    options?: ReadonlyStateOptions<T>
-): ReadonlyState<T> => {
-    const valueRef = { current: initialValue };
-    const { validator, errorHandler, validationErrorMessage } = options || {};
-
-    const proxy = new Proxy({} as T, objectHandler(valueRef));
-
-    const internalSet = createInternalSet(
-        valueRef,
-        validator,
-        errorHandler,
-        validationErrorMessage
-    );
-
-    return { value: proxy, internalSet };
-};
-
-const createPrimitive = <T>(
-    initialValue: T,
-    options?: ReadonlyStateOptions<T>
-): ReadonlyState<T> => {
-    const valueRef = { current: initialValue };
-    const { validator, errorHandler, validationErrorMessage } = options || {};
-
-    const internalSet = createInternalSet(
-        valueRef,
-        validator,
-        errorHandler,
-        validationErrorMessage
-    );
-
-    return { value: valueRef.current, internalSet };
+    const proxy = new Proxy(proxyKey, handler);
+    console.log('Target:', target);
+    console.log('Created Proxy (before set target):', proxy);
+    proxies.set(proxyKey, proxy);
+    console.log('Target:', target);
+    console.log('Created Proxy:', proxy);
+    return proxy as T;
 };
 
 const readonly = <T>(
     initialValue: T,
     options?: ReadonlyStateOptions<T>
 ): ReadonlyState<T> => {
-    if (isObject(initialValue)) {
-        return createProxy(initialValue as any, options);
-    } else {
-        return createPrimitive(initialValue, options);
-    }
+    let proxy = createDeepProxy(initialValue, options);
+    console.log('Created Proxy in readonly:', proxy);
+    const internalSet = (newValue: T) => {
+        if (options?.validator && !options.validator(newValue)) {
+            const error = new ValidationError(
+                options.validationErrorMessage ?? 'Validation failed.'
+            );
+            if (options?.errorHandler) {
+                options.errorHandler(error);
+                return;
+            }
+            throw error;
+        }
+        deepMerge(initialValue, newValue);
+        proxy = createDeepProxy(initialValue, options);
+    };
+
+    console.log('Proxy:', proxy);
+
+    return { value: proxy, internalSet };
 };
 
 export { readonly, DirectMutationError, ValidationError };
--- src/ImmuView.unit.spec.ts
+++ src/ImmuView.unit.spec.ts
@@ -1,36 +1,16 @@
 import { readonly, DirectMutationError, ValidationError } from './ImmuView'; // Adjust the import path
 
 describe('readonly utility', () => {
-    it('should initialize with a primitive value', () => {
-        const state = readonly(5);
-        expect(state.value).toBe(5);
-    });
-
     it('should initialize with an object', () => {
         const state = readonly({ count: 5 });
         expect(state.value.count).toBe(5);
     });
 
-    it('should throw an error when trying to modify a primitive', () => {
-        const state = readonly(5);
-        let temp = state as any;
-        expect(() => {
-            temp.value = 10;
-        }).toThrow(DirectMutationError);
-    });
-
     it('should throw an error when trying to modify an object property', () => {
         const state = readonly({ count: 5 });
-        let temp = state as any;
         expect(() => {
-            temp.count = 10;
+            (state.value as any).count = 10;
         }).toThrow(DirectMutationError);
-    });
-
-    it('should allow internal modification of a primitive', () => {
-        const state = readonly(5);
-        state.internalSet(10);
-        expect(state.value).toBe(10);
     });
 
     it('should allow internal modification of an object', () => {
@@ -40,151 +20,339 @@ describe('readonly utility', () => {
     });
 
     it('should not allow direct modification of nested object properties', () => {
-        const state = readonly({ data: { count: 5 } });
+        const state = readonly({ count: 5, nested: { value: 10 } });
         expect(() => {
-            state.value.data.count = 10;
+            delete (state.value.nested as any).value;
+            // state.value.count = 10;
+            //(state.value.nested as any).value = 20;
         }).toThrow(DirectMutationError);
     });
 
     it('should allow internal modification of nested object properties', () => {
-        const state = readonly({ data: { count: 5 } });
-        state.internalSet({ data: { count: 10 } });
-        expect(state.value.data.count).toBe(10);
+        const state = readonly({ count: 5, nested: { value: 10 } });
+        state.internalSet({ count: 5, nested: { value: 20 } });
+        expect(state.value.nested.value).toBe(20);
     });
 
-    it('should handle arrays', () => {
-        const state = readonly([1, 2, 3]);
-        expect(state.value[0]).toBe(1);
+    it('should handle deep proxying', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        value: 10,
+                    },
+                },
+            },
+        });
+        expect(state.value.level1.level2.level3.value).toBe(10);
     });
 
-    it('should not allow direct modification of array elements', () => {
-        const state = readonly([1, 2, 3]);
-        let temp = state as any;
+    it('should throw error on deep property modification', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        value: 10,
+                    },
+                },
+            },
+        });
         expect(() => {
-            temp.value[0] = 10;
+            (state.value.level1.level2.level3 as any).value = 20;
         }).toThrow(DirectMutationError);
     });
 
-    it('should allow internal modification of arrays', () => {
-        const state = readonly([1, 2, 3]);
-        state.internalSet([10, 20, 30]);
-        expect(state.value[0]).toBe(10);
+    it('should allow internal modification on deep properties', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        value: 10,
+                    },
+                },
+            },
+        });
+        state.internalSet({
+            level1: {
+                level2: {
+                    level3: {
+                        value: 20,
+                    },
+                },
+            },
+        });
+        expect(state.value.level1.level2.level3.value).toBe(20);
     });
 
-    it('should validate using a custom validator', () => {
-        const state = readonly(5, {
-            validator: (value) => value < 10,
-        });
-        state.internalSet(9);
-        expect(state.value).toBe(9);
-    });
-
-    it('should throw a validation error for invalid values', () => {
-        const state = readonly(5, {
-            validator: (value) => value < 10,
-        });
+    it('should validate using custom validator', () => {
+        const validator = (value: any) => value.count <= 10;
+        const state = readonly({ count: 5 }, { validator });
         expect(() => {
-            state.internalSet(11);
+            state.internalSet({ count: 15 });
         }).toThrow(ValidationError);
     });
 
-    it('should use a custom error handler', () => {
-        const mockErrorHandler = jest.fn();
-        const state = readonly(5, {
-            validator: (value) => value < 10,
-            errorHandler: mockErrorHandler,
-        });
-        state.internalSet(11);
-        expect(mockErrorHandler).toHaveBeenCalledWith(
-            expect.any(ValidationError)
-        );
-    });
-
-    it('should allow internalSet to modify the state', () => {
-        const state = readonly({ count: 5 });
+    it('should not throw error when validation passes', () => {
+        const validator = (value: any) => value.count <= 10;
+        const state = readonly({ count: 5 }, { validator });
         state.internalSet({ count: 10 });
         expect(state.value.count).toBe(10);
     });
 
-    it('should not modify the state when validation fails', () => {
+    it('should handle custom error messages', () => {
+        const validator = (value: any) => value.count <= 10;
+        const validationErrorMessage = 'Count exceeds limit';
         const state = readonly(
             { count: 5 },
-            {
-                validator: (newValue) => newValue.count < 10,
-            }
+            { validator, validationErrorMessage }
         );
-        state.internalSet({ count: 15 });
-        expect(state.value.count).toBe(5);
-    });
-
-    it('should call the errorHandler when validation fails', () => {
-        const mockErrorHandler = jest.fn();
-        const state = readonly(
-            { count: 5 },
-            {
-                validator: (newValue) => newValue.count < 10,
-                errorHandler: mockErrorHandler,
-            }
-        );
-        state.internalSet({ count: 15 });
-        expect(mockErrorHandler).toHaveBeenCalledWith(
-            expect.any(ValidationError)
-        );
-    });
-
-    it('should handle nested objects', () => {
-        const state = readonly({ outer: { inner: 5 } });
-        expect(state.value.outer.inner).toBe(5);
-    });
-
-    it('should throw an error when trying to modify a nested object', () => {
-        const state = readonly({ outer: { inner: 5 } });
         expect(() => {
-            (state.value.outer as any).inner = 10;
+            state.internalSet({ count: 15 });
+        }).toThrowError(new ValidationError('Count exceeds limit'));
+    });
+
+    it('should handle custom error handler', () => {
+        const errorHandler = jest.fn();
+        const validator = (value: any) => value.count <= 10;
+        const state = readonly({ count: 5 }, { validator, errorHandler });
+        state.internalSet({ count: 15 });
+        expect(errorHandler).toBeCalledWith(
+            new ValidationError('Validation failed.')
+        );
+    });
+});
+
+describe('readonly utility - edge cases', () => {
+    it('should handle arrays correctly', () => {
+        const state = readonly([1, 2, 3]);
+        expect(state.value[0]).toBe(1);
+        expect(() => {
+            (state.value as any)[0] = 4;
+        }).toThrow(DirectMutationError);
+        expect(() => {
+            (state.value as any).push(4);
         }).toThrow(DirectMutationError);
     });
 
-    it('should allow internalSet to modify a nested object', () => {
-        const state = readonly({ outer: { inner: 5 } });
-        state.internalSet({ outer: { inner: 10 } });
-        expect(state.value.outer.inner).toBe(10);
+    it('should handle functions correctly', () => {
+        const fn = () => 'Hello';
+        const state = readonly({ fn });
+        expect(state.value.fn()).toBe('Hello');
+        expect(() => {
+            (state.value as any).fn = () => 'Changed';
+        }).toThrow(DirectMutationError);
     });
 
+    it('should handle nested arrays correctly', () => {
+        const state = readonly({ data: [1, { value: 2 }, 3] });
+        expect((state.value.data[1] as { value: number }).value).toBe(2);
+        expect(() => {
+            (state.value.data[1] as any).value = 3;
+        }).toThrow(DirectMutationError);
+    });
+
+    it('should handle prototype chain correctly', () => {
+        const obj = Object.create({ inherited: 'value' });
+        const state = readonly(obj);
+        expect(state.value.inherited).toBe('value');
+        expect(() => {
+            (state.value as any).inherited = 'changed';
+        }).toThrow(DirectMutationError);
+    });
+
+    it('should handle null and undefined correctly', () => {
+        const state = readonly({ value: null, undef: undefined });
+        expect(state.value.value).toBeNull();
+        expect(state.value.undef).toBeUndefined();
+    });
+
+    it('should handle symbols correctly', () => {
+        const sym = Symbol('key');
+        const state = readonly({ [sym]: 'value' });
+        expect(state.value[sym]).toBe('value');
+        expect(() => {
+            (state.value as any)[sym] = 'changed';
+        }).toThrow(DirectMutationError);
+    });
+});
+
+describe('readonly utility extended tests', () => {
     it('should handle arrays', () => {
-        const state = readonly({ numbers: [1, 2, 3] });
-        expect(state.value.numbers[0]).toBe(1);
+        const state = readonly([1, 2, 3]);
+        expect(state.value[0]).toBe(1);
+        expect(() => {
+            (state.value as any)[0] = 4;
+        }).toThrow(DirectMutationError);
+        expect(() => {
+            (state.value as any).push(4);
+        }).toThrow(DirectMutationError);
     });
 
-    it('should throw an error when trying to modify an array', () => {
+    it('should handle nested arrays', () => {
         const state = readonly({ numbers: [1, 2, 3] });
+        expect(state.value.numbers[1]).toBe(2);
         expect(() => {
             (state.value.numbers as any).push(4);
         }).toThrow(DirectMutationError);
     });
 
-    it('should handle primitives', () => {
-        const state = readonly(5);
-        expect(state.value).toBe(5);
-    });
-
-    it('should not allow modification of primitives', () => {
-        const state = readonly(5);
+    it('should handle array of objects', () => {
+        const state = readonly([{ count: 1 }, { count: 2 }]);
+        expect(state.value[0].count).toBe(1);
         expect(() => {
-            (state as any).value = 10;
+            (state.value[0] as any).count = 3;
         }).toThrow(DirectMutationError);
     });
 
-    it('should allow internalSet to modify a primitive', () => {
-        const state = readonly(5);
-        state.internalSet(10);
-        expect(state.value).toBe(10);
+    it('should not allow adding new properties', () => {
+        const state = readonly({ count: 5 });
+        expect(() => {
+            (state.value as any).newProp = 10;
+        }).toThrow(DirectMutationError);
     });
 
-    it('should not modify a primitive when validation fails', () => {
-        const state = readonly(5, {
-            validator: (newValue) => newValue < 10,
-        });
-        state.internalSet(15);
-        expect(state.value).toBe(5);
+    it('should not allow deleting properties', () => {
+        const state = readonly({ count: 5 });
+        expect(() => {
+            delete (state.value as any).count;
+        }).toThrow(DirectMutationError);
     });
+
+    it('should handle objects with null values', () => {
+        const state = readonly({ prop: null });
+        expect(state.value.prop).toBeNull();
+    });
+
+    it('should handle objects with undefined values', () => {
+        const state = readonly({ prop: undefined });
+        expect(state.value.prop).toBeUndefined();
+    });
+
+    it('should handle objects with function values', () => {
+        const fn = () => 42;
+        const state = readonly({ func: fn });
+        expect(state.value.func()).toBe(42);
+    });
+
+    it('should not allow modifying functions', () => {
+        const fn = () => 42;
+        const state = readonly({ func: fn });
+        expect(() => {
+            (state.value as any).func = () => 24;
+        }).toThrow(DirectMutationError);
+    });
+
+    it('should handle Date objects', () => {
+        const date = new Date();
+        const state = readonly({ date });
+        expect(state.value.date).toBeInstanceOf(Date);
+    });
+
+    it('should not allow modifying Date objects', () => {
+        const date = new Date();
+        const state = readonly({ date });
+        expect(() => {
+            (state.value.date as any).setFullYear(2000);
+        }).toThrow(DirectMutationError);
+    });
+
+    // ... continue with other data types and scenarios
+
+    it('should handle deeply nested structures', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        level4: {
+                            value: 10,
+                        },
+                    },
+                },
+            },
+        });
+        expect(state.value.level1.level2.level3.level4.value).toBe(10);
+    });
+
+    it('should not allow modifications in deeply nested structures', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        level4: {
+                            value: 10,
+                        },
+                    },
+                },
+            },
+        });
+        expect(() => {
+            (state.value.level1.level2.level3.level4 as any).value = 20;
+        }).toThrow(DirectMutationError);
+    });
+
+    it('should handle arrays in deeply nested structures', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        values: [1, 2, 3],
+                    },
+                },
+            },
+        });
+        expect(state.value.level1.level2.level3.values[1]).toBe(2);
+    });
+
+    it('should not allow modifications in arrays of deeply nested structures', () => {
+        const state = readonly({
+            level1: {
+                level2: {
+                    level3: {
+                        values: [1, 2, 3],
+                    },
+                },
+            },
+        });
+        expect(() => {
+            (state.value.level1.level2.level3.values as any).push(4);
+        }).toThrow(DirectMutationError);
+    });
+
+    // ... continue with other edge cases
+
+    it('should handle internal modifications of arrays', () => {
+        const state = readonly([1, 2, 3]);
+        state.internalSet([4, 5, 6]);
+        expect(state.value[0]).toBe(4);
+    });
+
+    it('should handle internal modifications of nested arrays', () => {
+        const state = readonly({ numbers: [1, 2, 3] });
+        state.internalSet({ numbers: [4, 5, 6] });
+        expect(state.value.numbers[0]).toBe(4);
+    });
+
+    it('should handle internal modifications of array of objects', () => {
+        const state = readonly([{ count: 1 }, { count: 2 }]);
+        state.internalSet([{ count: 3 }, { count: 4 }]);
+        expect(state.value[0].count).toBe(3);
+    });
+
+    // ... continue with other scenarios for internal modifications
+
+    it('should handle symbols', () => {
+        const symbolKey = Symbol('key');
+        const state = readonly({ [symbolKey]: 42 });
+        expect(state.value[symbolKey]).toBe(42);
+    });
+
+    it('should not allow modifications using symbols', () => {
+        const symbolKey = Symbol('key');
+        const state = readonly({ [symbolKey]: 42 });
+        expect(() => {
+            (state.value as any)[symbolKey] = 24;
+        }).toThrow(DirectMutationError);
+    });
+
+    // ... continue with other data types and scenarios
 });
