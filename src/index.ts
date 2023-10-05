ed76f90c55de1c798dca9b271f56e25931e0f42b
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2023-10-05 11:38:02 +0200

    adapted exports due to more exports in ImmuView.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export * from './ImmuView';
+import { readonly } from './ImmuView';
+
+export default readonly;
+export {
+    // errors
+    DirectMutationError,
+    ValidationError,
+    ErrorHandler,
+
+    // utils
+    deepMerge,
+    isObject,
+
+    // types
+    ReadonlyState,
+    ReadonlyStateOptions,
+    Validator,
+
+    // utility - export again for convenience
+    readonly,
+} from './ImmuView';
